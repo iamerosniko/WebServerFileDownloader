e5ed3a94ecd854e4ad79c17e27fe6bc1deb94169
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-25 10:31:32 -0500

    up

--- WebServerFileDownloader/Sample.aspx
+++ WebServerFileDownloader/Sample.aspx
@@ -9,7 +9,24 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="UploadFile" />
+        <hr />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" EmptyDataText = "No files uploaded">
+            <Columns>
+                <asp:BoundField DataField="Text" HeaderText="File Name" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkDownload" Text = "Download" CommandArgument = '<%# Eval("Value") %>' runat="server" OnClick = "DownloadFile"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID = "lnkDelete" Text = "Delete" CommandArgument = '<%# Eval("Value") %>' runat = "server" OnClick = "DeleteFile" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
     </div>
     </form>
 </body>
--- WebServerFileDownloader/Sample.aspx.cs
+++ WebServerFileDownloader/Sample.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 namespace WebServerFileDownloader
 {
@@ -11,7 +12,37 @@ namespace WebServerFileDownloader
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                string[] filePaths = Directory.GetFiles(Server.MapPath("~/Server Folder/"));
+                List<ListItem> files = new List<ListItem>();
+                foreach (string filePath in filePaths)
+                {
+                    files.Add(new ListItem(Path.GetFileName(filePath), filePath));
+                }
+                GridView1.DataSource = files;
+                GridView1.DataBind();
+            }
+        }
+        protected void UploadFile(object sender, EventArgs e)
+        {
+            string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+            FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Server Folder/") + fileName);
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+        protected void DownloadFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            Response.ContentType = ContentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(filePath));
+            Response.WriteFile(filePath);
+            Response.End();
+        }
+        protected void DeleteFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            File.Delete(filePath);
+            Response.Redirect(Request.Url.AbsoluteUri);
         }
     }
 }
